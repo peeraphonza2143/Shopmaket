400e55034cd8f90a78e7f334764c71452dbe0e0e
peeraphonza2143 <53931459+peeraphonza2143@users.noreply.github.com>
2021-05-28 16:10:34 +0700

    2

---
+++ admin.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include('h.php');?>
+    <head>
+    <body>
+        <div class="container">
+        <?php include('navbar.php');?>
+        <p></p>
+    <div class="row">
+      <div class="col-md-3">
+        <?php include('menu_left.php');?>
+    </div>
+    <div class="col-md-6">
+        <a href="admin.php?act=add" class="btn-info btn-sm">เพิ่ม</a>
+        <p></p>
+        <?php
+            $act = $_GET['act'];
+            if($act == 'add'){
+            include('admin_form_add.php');
+            }elseif ($act == 'edit') {
+            //include('edit_product.php');
+        }
+            else {
+            include('admin_list.php');
+        }
+        ?>
+    </div>
+    </div>
+  </div>
+  </body>
+</html>
